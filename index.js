f57b9a2495a23835661c36f1dda20789c9e0a776
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-08-19 13:04:07 -0500

    added generateSVG function and added generateSVG function call in the async generateLogo function

--- index.js
+++ index.js
@@ -34,9 +34,40 @@ const questions = [
     },
 ];
 
+function generateSVG(answers) {
+    let svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
+
+      // Handle the chosen shape
+  if (answers.shapes === 'Circle') {
+    svg += `<circle cx="150" cy="100" r="80" fill="${answers.shapes_color}" />`;
+  } else if (answers.shapes === 'Square') {
+    svg += `<rect x="50" y="50" width="200" height="200" fill="${answers.shapes_color}" />`;
+  } else if (answers.shapes === 'Triangle') {
+    svg += `<path d="M150 20 L250 180 L50 180 Z" fill="${answers.shapes_color}" />`;
+  }
+
+  // Add the text
+  svg += `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.text_color}">${answers.text}</text>`;
+
+  svg += `</svg>`;
+  return svg;
+}
+
+
 async function generateLogo () {
     const answers = await inquirer.prompt(questions);
     console.log(answers);
+
+    const svg = generateSVG(answers);
+
+    // Write the SVG string to a file named 'logo.svg'
+    fs.writeFile('logo.svg', svg, (err) => {
+        if (err) {
+            console.error('Error writing to file:', err);
+        } else {
+            console.log('Logo SVG file created successfully!');
+        }
+    });
 }
 
 generateLogo();
