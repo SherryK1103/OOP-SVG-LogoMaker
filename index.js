44136336f3751bf14020278b6850df6d2219295a
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-08-18 19:56:09 -0500

    added index js code plus shapes and commented out irrelevent code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [ 
@@ -34,92 +33,98 @@ const questions = [
     },
 ];
 
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        console.log('writing file');
-
-        if (err) {
-            console.error('Error writing README file:', err);
-        } else {
-            console.log('README file created successfully!');
-        }
-    });
+async function generateLogo () {
+    const answers = await inquirer.prompt(questions);
+    console.log(answers);
 }
 
-// TODO: Create a function to initialize app
-function init() {
-    inquirer
-    .prompt(questions)
-    .then((answers) => {
-        console.log('answers ---> ',answers);
-        writeToFile('README.md', generateReadmeContent(answers));
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-          // Prompt couldn't be rendered in the current environment
-        } else {
-          // Something else went wrong
-        }
-    });
-}
+generateLogo();
 
-function getLicenseBadgeURL(license) {
-    const licenseBadges = {    
-        "Apache License 2.0": "https://img.shields.io/badge/License-Apache%202.0-blue.svg",
-        "GNU GPLv3": "https://img.shields.io/badge/License-GPLv3-blue.svg",
-        "MIT License": "https://img.shields.io/badge/License-MIT-yellow.svg",
-    };
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data, (err) => {
+//         console.log('writing file');
+
+//         if (err) {
+//             console.error('Error writing README file:', err);
+//         } else {
+//             console.log('README file created successfully!');
+//         }
+//     });
+// }
+
+// // TODO: Create a function to initialize app
+// function init() {
+//     inquirer
+//     .prompt(questions)
+//     .then((answers) => {
+//         console.log('answers ---> ',answers);
+//         writeToFile('README.md', generateReadmeContent(answers));
+//     })
+//     .catch((error) => {
+//         if (error.isTtyError) {
+//           // Prompt couldn't be rendered in the current environment
+//         } else {
+//           // Something else went wrong
+//         }
+//     });
+// }
+
+// function getLicenseBadgeURL(license) {
+//     const licenseBadges = {    
+//         "Apache License 2.0": "https://img.shields.io/badge/License-Apache%202.0-blue.svg",
+//         "GNU GPLv3": "https://img.shields.io/badge/License-GPLv3-blue.svg",
+//         "MIT License": "https://img.shields.io/badge/License-MIT-yellow.svg",
+//     };
     
-    return licenseBadges[license];
-    }
+//     return licenseBadges[license];
+//     }
 
 
-function generateReadmeContent(answers) {
-    const licenseBadge = getLicenseBadgeURL(answers.license);
+// function generateReadmeContent(answers) {
+//     const licenseBadge = getLicenseBadgeURL(answers.license);
 
-    return `
-# ${answers.title}
+//     return `
+// # ${answers.title}
 
-## Description
-${answers.description}
+// ## Description
+// ${answers.description}
 
-## Table of Contents
-${answers.Table_of_Contents}
+// ## Table of Contents
+// ${answers.Table_of_Contents}
 
-## Installation
-${answers.installation}
+// ## Installation
+// ${answers.installation}
 
-## Usage
-${answers.usage}
+// ## Usage
+// ${answers.usage}
 
-## License
-${answers.license}
-![License Badge](${licenseBadge})
+// ## License
+// ${answers.license}
+// ![License Badge](${licenseBadge})
 
-## Contributing
-${answers.contributing}
+// ## Contributing
+// ${answers.contributing}
 
-## Tests
-${answers.tests}
-`;
-}
+// ## Tests
+// ${answers.tests}
+// `;
+// }
 
-function init() {
-    inquirer
-        .prompt(questions)
-        .then((answers) => {
-            writeToFile('README.md', generateReadmeContent(answers));
-        })
-        .catch((error) => {
-            if (error.isTtyError) {
-                console.error("Prompt couldn't be rendered in the current environment.");
-            } else {
-                console.error("Something else went wrong:", error);
-            }
-        });
-}
+// function init() {
+//     inquirer
+//         .prompt(questions)
+//         .then((answers) => {
+//             writeToFile('README.md', generateReadmeContent(answers));
+//         })
+//         .catch((error) => {
+//             if (error.isTtyError) {
+//                 console.error("Prompt couldn't be rendered in the current environment.");
+//             } else {
+//                 console.error("Something else went wrong:", error);
+//             }
+//         });
+// }
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
